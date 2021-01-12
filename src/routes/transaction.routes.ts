1ef14d3fb6057abf48a8061caef9d6874d1ccb01
carlos-eugenio <carlos-eugenio@live.com>
2021-01-12 09:12:39 -0300

    feature completed: create and list transactions - criar e listar transaçoes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,48 @@
 import { Router } from 'express';
+import Transaction from '../models/Transaction';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
+// List all transactions
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
+// Income / Outcome
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // Recebe dados usuário
+    const { title, value, type } = request.body;
+
+    // Cria o novo serviço e passa o repositorio pra ele
+    const CreateTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // Executa o servico (execute()) passando os valores recebidos na requisição
+    const transaction = CreateTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    // Retorna a transação
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
